5e47d6a79ff970cb550f92b3f8c4096d05542da9
Maylee <mgagnon987@gmail.com>
2019-11-10 16:59:28 -0500

    works with mixed case, spaces, random symbols/numbers
    issue before: rightPos was -- instead of ++

--- Palindrome/src/Palindrome.cpp
+++ Palindrome/src/Palindrome.cpp
@@ -8,65 +8,57 @@
 #include <stdlib.h>
 using namespace std;
 
-/* Start at either end, checks if the same, if not return false, if yes then recursive call rightPoss++, leftPos--,
- * if middle aka base case(?) if right++ >=leftpos
- * 0 1 2 3 4 5   05 14 23
- * 0 1 2 3 4 5 6  06 15 24 3
- * 0 1 2
+/* Recursively checks whether a given string is a palindrome
  *
+ * Starts at either end, gets letters, checks if the same, if not return false, if yes then recursive call rightPoss++, leftPos--,
+ * if middle which means reached the last comparison (right++ >=leftpos) then return from function
+ * Some examples checked: mom, abc, racecar, race car, mo m, m om, RaCEaRE, Never odd or even, A man, a plan, a canal: Panama!
+ *
+ * 0 1 2 3 4 5   05 14 23  > (2+1)>=3
+ * 0 1 2 3 4 5 6    06 15 24 3 > (3+1)>=3
  */
 bool checkPalindrome(char* str, int rightPos, int leftPos) {
-	bool isEqual;
-	 cout << rightPos << " " << leftPos << endl;
-
 	char rChar = str[rightPos];
+	char lChar = str[leftPos];
 	while(isalpha(rChar) == 0) { // non-letter
-		rightPos--;
+		rightPos++;
 		rChar = str[rightPos];
 	}
-
-	char lChar = str[leftPos];
-	while(isalpha(lChar) == 0) { // non-letter
+	while(isalpha(lChar) == 0) {
 		leftPos--;
 		lChar = str[leftPos];
 	}
 
-	if (rChar == lChar) {
-		//cout << rChar << " " << lChar << " true" << endl;
-		isEqual = true;
-	}
-	else if (rChar != lChar){
-		cout << "false" << endl;
+	if (rChar != lChar){
 		return false; //pair are not equal so return false
 	}
-
 	if ((rightPos+1) >= leftPos) { // base case, reached middle, all letters have been checked
 		return true;
 	}
 	else {
 		rightPos++;
 		leftPos--;
-	checkPalindrome(str,rightPos,leftPos);
+		checkPalindrome(str,rightPos,leftPos);
 	}
 }
 
 int main() {
-	cout << "Hello" << endl;
-
 	cout << "Enter phrase to check" << endl;
 	char str[256];
 	fgets(str, 256,stdin);
 
-	cout << "String: " << str << endl;
+	cout << "Phrase Entered: " << str << endl;
 
-	// change to lower case, to ignore case
-	for (int i = 0; i < strlen(str); i++){
+	for (int i = 0; i < strlen(str); i++){ // change to lower case, to ignore case
 		str[i] = tolower(str[i]);
 	}
-	cout << "String: " << str << endl;
 
 	bool result = checkPalindrome(str,0, (strlen(str)-2)); // avoiding null terminator, check for letters anyways though
-	cout << "result: " << result << endl;
-
+	if (result) {
+		cout << "Yes, Phrase entered is a palindrome" << endl;
+	}
+	else {
+		cout << "No, Phrase entered is not a palindrome" << endl;
+	}
 	return 0;
 }
